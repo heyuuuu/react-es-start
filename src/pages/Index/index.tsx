f9b06a8fd120ce7b0627c2787fb6b1e4ca6252d7
heyu <664475324@qq.com>
2020-12-31 18:10:01 +0800

    fps

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,10 +1,39 @@
-import React from "react"
+import React, { createRef , RefObject} from "react"
 import { Navigation } from "src/utils"
+import style from "./index.less"
 export default class Index extends React.Component {
+    protected showNode: RefObject<HTMLDivElement>
+    constructor(props: Record<string,unknown>){
+        super(props)
+        this.showNode = createRef()
+        this.requestAnimationFrame = this.requestAnimationFrame.bind(this)
+    }
     componentDidMount(){
         console.log(Navigation.GetPathFromName('home',{id: 5}))
     }
+    requestAnimationFrame(){
+        const palceWidth = 300
+        const showNode = this.showNode.current
+        let timer
+        const calculate = () => {
+            const width = showNode.style.width || '0'
+            if(parseInt(width) < palceWidth){
+                showNode.style.width = parseInt(width) + 5 + 'px';
+                showNode.innerHTML =     parseInt(width)/palceWidth*100 + '%';
+                timer = window.requestAnimationFrame(calculate);
+            }else{
+                cancelAnimationFrame(timer);
+            }
+        }
+        timer = window.requestAnimationFrame(calculate)
+    }
     render(){
-        return <>this is <span onClick={Navigation.pushCall('home',{id: 4})}>Index</span></>
+        return <div className={style.container}>
+            <p>
+                <span onClick={Navigation.pushCall('home',{id: 4})}>home</span>
+                <span onClick={this.requestAnimationFrame}>动画GO</span>
+            </p>
+            <div className={style.frame} ref={this.showNode}></div>
+        </div>
     }
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,23 @@
 import React , { Component } from "React"
+import { Route , RouteChildrenProps } from "react-router-dom"
 // import { Navigation } from "src/utils"
 
-class Home extends Component {
+// 单例模式
+function Singleton(){
+	return <div>单例模式</div>
+}
+
+class Home extends Component<RouteChildrenProps> {
 	componentDidMount(){
-		// const path = Navigation.CreateFullPath('homes',{id: 3},{modal: "5"})
-		// console.log(path)
+		console.log
 	}
 	public render(){
-		return <div>home</div>
+		const { match } = this.props
+		return <div>设计模式
+			<div>
+				<Route path={match.path + "/singleton"} component={Singleton} />
+			</div>
+		</div>
 	}
 }
 
