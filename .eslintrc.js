a6cbdadc28683d2c7dc728535b23b213839f7ae2
heyu <664475324@qq.com>
2021-04-09 18:21:52 +0800

    添加进度

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,20 +11,21 @@ module.exports = {
 		"plugin:react/recommended",
 		"plugin:@typescript-eslint/recommended"
 	],
-	globals:{
-		"document": true,
-		"localStorage": true,
-		"window": true
-	},
-	parserOptions: {
-		"ecmaVersion": 7,
-		"sourceType": "module"
-	},
+	// globals:{
+	// 	"document": true,
+	// 	"localStorage": true,
+	// 	"window": true
+	// },
+	// parserOptions: {
+	// 	"ecmaVersion": 7,
+	// 	"sourceType": "module"
+	// },
 	rules: {
-		"no-irregular-whitespace": 0, // 不规则的空格
-		"no-useless-escape": "error", // 禁止不必要的转义使用
+		// "no-irregular-whitespace": 0, // 不规则的空格
+		// "no-useless-escape": "error", // 禁止不必要的转义使用
 		"no-var": "error",
-		// "@typescript-eslint/explicit-module-boundary-types": "off",
-		"react/prop-types": [2, { ignore: ['children'] }]
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+		"indent": ["error", 4]
+		// "react/prop-types": [2, { ignore: ['children'] }]
 	}
 }
--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,8 @@ import React from "react"
 
 const router: Array<RouteItem> = [
 	{name: "home",path: "/home",component: React.lazy(() => import("src/pages/Manager"))},
-	{name: "setting",path: "/setting",component: React.lazy(() => import("src/pages/Setting"))}
+	{name: "setting",path: "/setting",component: React.lazy(() => import("src/pages/Setting"))},
+	{name: "progress",path: "/progress",component: React.lazy(() => import("src/pages/Progress"))},
 ]
 
 export default router
