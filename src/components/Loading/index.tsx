11ba93f2e4dd85434b6565c09a532ba74184d114
heyu <664475324@qq.com>
2021-04-12 17:02:46 +0800

    优化加载

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,22 +1,9 @@
-import React from "react";
-import Backdrop from "@material-ui/core/Backdrop";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-	backdrop: {
-		zIndex: theme.zIndex.drawer + 1,
-		color: "#fff",
-	},
-}))
+import React from "react"
 
 export default function SimpleBackdrop() {
-	const classes = useStyles()
 	return (
 		<div>
-			<Backdrop className={classes.backdrop} open>
-				<CircularProgress color="inherit" />
-			</Backdrop>
+			加载中
 		</div>
 	);
 }
