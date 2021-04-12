f6c5cf8683fd7d670d7d4e53d0ad8d0218b1339b
heyu <664475324@qq.com>
2021-04-12 15:50:32 +0800

    添加规范

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,9 @@
 import React from "react"
 
 const router: Array<RouteItem> = [
-	{name: "home",path: "/home",component: React.lazy(() => import("src/pages/Manager"))},
-	{name: "setting",path: "/setting",component: React.lazy(() => import("src/pages/Setting"))},
-	{name: "progress",path: "/progress",component: React.lazy(() => import("src/pages/Progress"))},
+	{name: "home", path: "/home", component: React.lazy(() => import("src/pages/Manager"))},
+	{name: "setting", path: "/setting", component: React.lazy(() => import("src/pages/Setting"))},
+	{name: "progress", path: "/progress", component: React.lazy(() => import("src/pages/Progress"))},
 ]
 
 export default router
